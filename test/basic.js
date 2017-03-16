444fd9dc15f0eb33616df67d6737d51677ead1e4
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2017-03-15 21:11:27 -0400

    style: standard --fix
    All changes are from running `standard --fix`

--- test/basic.js
+++ test/basic.js
@@ -1,23 +1,23 @@
-var client = require('../lib/simple-queue').Client({ debug: false });
+var client = require('../lib/simple-queue').Client({ debug: false })
 
-client.subscribe('QueueA', function(err, message) {
-  console.log('QueueA Message Received');
-  console.log(message);
-});
+client.subscribe('QueueA', function (err, message) {
+  console.log('QueueA Message Received')
+  console.log(message)
+})
 
-client.subscribe('QueueB', function(err, message) {
-  console.log('QueueB Message Received');
-  console.log(message);
-});
+client.subscribe('QueueB', function (err, message) {
+  console.log('QueueB Message Received')
+  console.log(message)
+})
 
-client.subscribe('QueueB', function(err, message) {
-  console.log('QueueB Message Received (2)');
-  console.log(message);
-});
+client.subscribe('QueueB', function (err, message) {
+  console.log('QueueB Message Received (2)')
+  console.log(message)
+})
 
-setInterval(function() {
-  client.enqueue('QueueA', { something: "someValueA" }, function(err, result) {
-    console.log('Error: ' + err);
-    console.log(result);
-  });
-}, 500);
+setInterval(function () {
+  client.enqueue('QueueA', { something: 'someValueA' }, function (err, result) {
+    console.log('Error: ' + err)
+    console.log(result)
+  })
+}, 500)
