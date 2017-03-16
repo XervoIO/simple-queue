2b1cd461f0088fdbccaeff314a186fb2e820311f
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2017-03-15 21:18:53 -0400

    style: fix remaining standard linting issues

--- test/basic.js
+++ test/basic.js
@@ -1,16 +1,19 @@
 var client = require('../lib/simple-queue').Client({ debug: false })
 
 client.subscribe('QueueA', function (err, message) {
+  if (err) console.log('test error', err)
   console.log('QueueA Message Received')
   console.log(message)
 })
 
 client.subscribe('QueueB', function (err, message) {
+  if (err) console.log('test error', err)
   console.log('QueueB Message Received')
   console.log(message)
 })
 
 client.subscribe('QueueB', function (err, message) {
+  if (err) console.log('test error', err)
   console.log('QueueB Message Received (2)')
   console.log(message)
 })
